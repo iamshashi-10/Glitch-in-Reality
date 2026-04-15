f75ad34dcde781e0312b38085a6e8a2f21d47fe0
srishtipr <psrishti45@gmail.com>
2026-04-15 19:44:40 +0530

    Launch and buttons added

---
+++ Assets/Srishti/script_storage/RestartScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour
+{
+    public void RestartScene()
+    {
+        // 1. Clear any weird states
+        Time.timeScale = 1f; 
+        
+        // 2. Reload the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+        // 3. Optional: Manually clear the console via code if you want
+        Debug.ClearDeveloperConsole(); 
+    }
+}
