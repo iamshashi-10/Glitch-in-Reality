80031436d0ff3a0fa81a324757b0959cda2376ff
dpatil23 <darshvp23@iitk.ac.in>
2026-04-21 23:28:06 +0530

    Now finally info button working

--- Assets/DARSH/Scripts/UIToggle.cs
+++ Assets/DARSH/Scripts/UIToggle.cs
@@ -2,20 +2,10 @@ using UnityEngine;
 
 public class UIToggle : MonoBehaviour
 {
-    [Header("The Panel to Toggle")]
-    public GameObject targetPanel;
-
-    // This function will be triggered by your button
-    public void ToggleVisibility()
+    public GameObject captionText;
+    public void TurnOnCaption()
     {
-        // First, check to make sure you actually assigned a panel in the Inspector
-        if (targetPanel != null)
-        {
-            // Find out if the panel is currently turned on or off
-            bool isCurrentlyOn = targetPanel.activeSelf;
-
-            // Set the panel to the exact OPPOSITE of what it currently is
-            targetPanel.SetActive(!isCurrentlyOn);
-        }
+        // Toggle ON/OFF
+        captionText.SetActive(!captionText.activeSelf);
     }
 }
