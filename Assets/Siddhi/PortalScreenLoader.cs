81e49392064146fd088b5efaf071a9d104789b55
Shashi B <shashi123bhidodiya@gmail.com>
2026-04-23 23:26:19 +0530

    interaction done

--- Assets/Siddhi/PortalScreenLoader.cs
+++ Assets/Siddhi/PortalScreenLoader.cs
@@ -27,8 +27,24 @@ public class PortalSceneLoader : MonoBehaviour
         {
             // Hide/Disable the player so it doesn't interfere with the new scene's camera
             other.gameObject.SetActive(false);
-            
+
             SceneManager.LoadScene(sceneToLoad);
         }
+    }
+    public void LoadBlackhole()
+    {
+        SceneManager.LoadScene("Black_hole");
+    }
+    public void LoadAsteroid()
+    {
+        SceneManager.LoadScene("Asteroid");
+    }
+    public void LoadBb()
+    {
+        SceneManager.LoadScene("beforeBisphot");
+    }
+    public void LoadOM()
+    {
+        SceneManager.LoadScene("Orbital_Mechanics");
     }
 }
